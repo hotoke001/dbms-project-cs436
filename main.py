8fe0f0d9d05f6069854b7028fb832ee7ea4b4782
hotoke001 <71043228+hotoke001@users.noreply.github.com>
2021-12-08 20:44:45 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+import pymysql
+import pymysql.cursors
+import tkinter as tk
+from tkinter import *
+
+
+class Hospital(Frame):
+
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.grid()
+        self.create_widgets()
+        self.master = master
+        zip = StringVar()
+
+    def create_widgets(self):
+        self.button = Button(self, text="Open Database", fg="green", command=self.begin)
+        self.button.grid(row=0, column=0, sticky=W)
+
+        self.button2 = Button(self, text="Close Connection", fg="red", command=self.closed)
+        self.button2.grid(row=0, column=5, sticky=W)
+
+        self.button3 = Button(self, text="Add new Patient", command=self.insert_patients)
+        self.button3.grid(row=0, column=4, sticky=W)
+
+        self.button4 = Button(self, text="Doctor Staff Members", command=self.doctorlist)
+        self.button4.grid(row=0, column=2, sticky=W)
+
+        self.button5 = Button(self, text="All patients", command=self.patientlist)
+        self.button5.grid(row=0, column=3, sticky=W)
+
+    def begin(self):
+        # Establish a MySQL connection
+        self.con = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='DbTest123',
+                                   database='hdbms')
+        # Get the cursor to traverse the database, line by line
+        self.cur = self.con.cursor()
+
+    def closed(self):
+        self.con.commit()
+        self.cur.close()
+        self.con.close()
+
+    # Close MySQL Connections
+
+    def patientlist(self):
+        self.cur.execute('Select pname, gender, phonenumber, dob FROM patients')
+        print(self.cur.fetchall())
+
+    def insert_patients(self):  # Add an Patient
+
+        pname = input("Enter New Patient: ").strip()
+        gender = input("Enter Gender: ").strip()
+        phonenumber = input("Enter Phone Number: ").strip()
+        dob = input("Enter Date of Birth: ")
+        addyID = input("Enter Address Code:")
+
+        self.cur.execute(
+            "INSERT INTO patients (pname, gender,phonenumber,dob,adrID) VALUES (%s, %s, %s, %s,%s)",
+            (pname, gender, phonenumber, dob, addyID),
+        )
+        self.con.commit()
+        print()
+        return
+
+    def doctorlist(self):
+        new = Toplevel(root)
+        new.geometry("750x250")
+        new.title("New Window")
+
+        self.cur.execute('Select * from doctor')
+
+
+
+
+root = tk.Tk()
+root.title("Hospital Database Management")
+root.geometry('600x300')
+app = Hospital(root)
+root.mainloop()
